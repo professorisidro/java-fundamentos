b2eb7683a9d12f56513c45bcac3693993d7d42e7
professorisidro <isidro@professorisidro.com.br>
2022-02-03 12:00:20 -0300

    exemplos de IF

---
+++ Uri1035.java
@@ -0,0 +1,31 @@
+/*
+Leia 4 valores inteiros A, B, C e D. A seguir, 
+(1) se B for maior do que C  e                              B > C
+(2) se D for maior do que A, e                              D > A 
+(3) a soma de C com D for maior que a soma de A e B e   C + D > A + B
+(4) se C e D, ambos, forem positivos e                  C > 0 &&  D > 0
+(5) se a variável A for par                               A % 2 == 0
+*/
+
+import java.util.Scanner;
+public class Uri1035{
+    public static void main(String args[]){
+        Scanner teclado = new Scanner(System.in);
+        int A,B,C,D;
+
+        // entrada
+        A = teclado.nextInt();
+        B = teclado.nextInt();
+        C = teclado.nextInt();
+        D = teclado.nextInt();
+        //      1          2              3                    4                 5
+        if ( (B > C) && (D > A) && (C + D > A + B) &&  (C > 0 &&  D > 0) && (A % 2 == 0)){
+            System.out.println("Valores aceitos");
+        }
+        else{
+            System.out.println("Valores nao aceitos");
+        }
+
+        teclado.close();
+    }
+}
